e0e8721932445228ca7aa5198ce6704bbf649fac
Qaisar Raza <razaqaisar53@gmail.com>
2023-08-05 23:09:00 +0530

    Initial Commit

---
+++ CricKit/CricKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricKitApp.swift
+//  CricKit
+//
+//  Created by Qaisar Raza on 05/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CricKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
