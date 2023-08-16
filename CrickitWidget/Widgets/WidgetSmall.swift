88b81f483da14349d926323534eef7c864682756
Ephrim daniel J <mirhpe17@gmail.com>
2023-08-16 21:39:11 +0530

    changes for static medium widget

--- CrickitWidget/Widgets/WidgetSmall.swift
+++ CrickitWidget/Widgets/WidgetSmall.swift
@@ -16,35 +16,7 @@ struct WidgetSmall: View {
         self.liveScorecard = _liveScorecard
     }
     var body: some View {
-        VStack {
-            HStack {
-                Image(liveScorecard.liveScore[0].flagImageName)
-                    .resizable()
-                .frame(width: 15, height: 15)
-                Text(liveScorecard.liveScore[0].name)
-                    .fontWeight(.medium)
-            }
-            
-            HStack{
-                Text("\(liveScorecard.liveScore[0].runs)/\(liveScorecard.liveScore[0].wickets) in")
-                Text("\(liveScorecard.liveScore[0].overs)")
-                    .font(.caption)
-            }
-            
-            HStack {
-                Image(liveScorecard.liveScore[1].flagImageName)
-                    .resizable()
-                .frame(width: 15, height: 15)
-                Text(liveScorecard.liveScore[1].name)
-                    .fontWeight(.medium)
-            }
-            HStack{
-                Text("\(liveScorecard.liveScore[1].runs)/\(liveScorecard.liveScore[1].wickets) in")
-                Text("\(liveScorecard.liveScore[1].overs)")
-                    .font(.caption)
-            }
-            
-        }.padding()
+        TeamViewSmall(liveScorecard: liveScorecard)
     }
 }
 
@@ -53,5 +25,29 @@ struct WidgetSmall_Previews: PreviewProvider {
         WidgetSmall(_liveScorecard: LiveScoreCard(matchName: "India vs Australia", matchStatus: "India won by 100 Runs", playerOfTheMatch: "Virat Kohli", liveScore: [LiveScore(name: "IND", flagImageName: "teamInd", runs: 280, wickets: 6, overs: 50), LiveScore(name: "AUS", flagImageName: "teamAus", runs: 180, wickets: 5, overs: 40)]))
         
             .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
+
+struct TeamViewSmall: View {
+    
+    var liveScorecard: LiveScoreCard
+    
+    var body: some View {
+        VStack {
+            ForEach(liveScorecard.liveScore) { item in
+                HStack {
+                    Image(item.flagImageName)
+                        .resizable()
+                    .frame(width: 15, height: 15)
+                    Text(item.name)
+                        .fontWeight(.medium)
+                }
+                HStack{
+                    Text("\(item.runs)/\(item.wickets) in")
+                    Text("\(item.overs)")
+                        .font(.caption)
+                }
+            }
+        }.padding()
     }
 }
