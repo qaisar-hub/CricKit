15befd82ffdc77c78f9fb4fda7fbd71c82898432
Ephrim daniel J <mirhpe17@gmail.com>
2023-09-16 15:40:02 +0530

    Live Mode updating the missing changes

---
+++ CricKit/Views/New Group/LiveMatches.swift
@@ -0,0 +1,46 @@
+//
+//  LiveMatches.swift
+//  CricKit
+//
+//  Created by  Ephrim Daniel J on 16/09/23.
+//
+
+import SwiftUI
+
+struct LiveMatches: View {
+
+    @ObservedObject var liveScoreCardViewModel = LiveScoreCardViewModel()
+    @EnvironmentObject private var appSettings: AppSettings
+    var width = UIScreen.main.bounds.width
+
+    var body: some View {
+        VStack {
+            ScrollView(.vertical, showsIndicators: false){
+                VStack{
+                    if liveScoreCardViewModel.isLoading {
+                        LiveScoreCardShimmerView(width: width - 32)
+                            .offset(x: 16)
+                    } else {
+                        ForEach(Array(liveScoreCardViewModel.liveScoreCardlists.enumerated()), id: \.element.id) { index, liveScoreItem in
+                            NavigationLink(destination: LiveModePage(matchHeader: liveScoreItem.matchHeader, matchStatus: liveScoreItem.matchStatus)) {
+                                LiveScoreCardView(isLiveMatches: true, liveScoreCardData: liveScoreItem)
+                                    .frame(width: width - 32, height: 160)
+                                    .clipShape(LiveMatchCard())
+                                    .background(BlurManagerData.blurMaterial(colorScheme: appSettings.isDarkMode ? .dark : .light))
+                                    .clipShape(LiveMatchCard())
+                                    .cornerRadius(10, corners: .allCorners)
+                            }.accentColor(Color.gray)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    init() {
+        liveScoreCardViewModel.getLiveScore()
+    }
+}
+
+#Preview {
+    LiveMatches()
+}
