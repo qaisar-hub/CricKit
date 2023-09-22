d77cd14d0647a80f1f2af7d101544b56bac89cea
Qaisar Raza <razaqaisar53@gmail.com>
2023-09-22 07:20:34 +0530

    UI changes for Ranking View

--- CricKit/Views/New Group/Home/HomeSections/Ranking/ICCRankingView.swift
+++ CricKit/Views/New Group/Home/HomeSections/Ranking/ICCRankingView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ICCRankingView: View {
     @ObservedObject var rankingViewModel = RankingViewModel()
+    @EnvironmentObject private var appSettings: AppSettings
     var selectedTab: TabSelection
     var rankingArray: [Ranking]
     
@@ -28,7 +29,7 @@ struct ICCRankingView: View {
     var body: some View {
         NavigationStack {
             List {
-                Section(header: Text(sectionHeader)) { 
+                Section(header: HeaderView(title: sectionHeader, rightText: "Rating")) {
                     ForEach(0..<10) { index in
                         let rank = selectedTab != .team ? rankingArray[index].rank : rankingArray[index].rank
                         let symbolName = selectedTab != .team ? "person.crop.circle.fill" : "square.fill"
@@ -41,8 +42,27 @@ struct ICCRankingView: View {
                                          title: title ?? "",
                                          subtitle: subtitle ?? "")
                     }
-                }
+                }.listRowBackground(BlurManagerData.blurMaterial(colorScheme: appSettings.isDarkMode ? .dark : .light))
             }
+        }
+    }
+}
+
+struct HeaderView: View {
+    @EnvironmentObject private var appSettings: AppSettings
+    var title: String
+    var rightText: String
+
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(ColorManager.appTextColor(colorScheme: appSettings.isDarkMode ? .dark : .light))
+            Spacer()
+            Text(rightText)
+                .font(.caption)
+                .foregroundColor(ColorManager.appTextColor(colorScheme: appSettings.isDarkMode ? .dark : .light))
+                .fontWeight(.medium)
         }
     }
 }
@@ -55,23 +75,29 @@ struct RankingEntryView: View {
     var symbolName: String
     var title: String
     var subtitle: String
+    @EnvironmentObject private var appSettings: AppSettings
     
     var body: some View {
         HStack(spacing: 20) {
             Text("\(rank)")
                 .font(.headline)
+                .foregroundColor(ColorManager.appTextColor(colorScheme: appSettings.isDarkMode ? .dark : .light))
             Image(systemName: symbolName)
                 .resizable()
+                .foregroundStyle(.gray)
                 .frame(width: 30, height: 30)
             HStack {
                 VStack(alignment: .leading) {
                     Text(title)
                         .font(.headline)
+                        .foregroundColor(ColorManager.appTextColor(colorScheme: appSettings.isDarkMode ? .dark : .light))
                     Text(subtitle)
                         .font(.subheadline)
+                        .foregroundColor(ColorManager.appTextColor(colorScheme: appSettings.isDarkMode ? .dark : .light))
                 }
                 Spacer()
                 Text("\(rating)")
+                    .foregroundColor(ColorManager.appTextColor(colorScheme: appSettings.isDarkMode ? .dark : .light))
                     .font(.subheadline)
                     .multilineTextAlignment(.trailing)
             }
--- CricKit/Views/New Group/Home/HomeSections/Ranking/RankingDetailsView.swift
+++ CricKit/Views/New Group/Home/HomeSections/Ranking/RankingDetailsView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 enum TabSelection: String, CaseIterable, Identifiable {
-    case batsman, bowler, allRounder, team
+    case batsman = "Batsman", bowler = "Bowler", allRounder = "All Rounder", team = "Team"
     
     var id: String { self.rawValue }
 }
@@ -16,6 +16,7 @@ enum TabSelection: String, CaseIterable, Identifiable {
 struct RankingDetailsView: View {
     @State private var selectedTabIndex = 0
     @ObservedObject var rankingViewModel = RankingViewModel()
+    @EnvironmentObject private var appSettings: AppSettings
     var matchFormat: String
     
     init(matchFormat: String) {
@@ -45,35 +46,40 @@ struct RankingDetailsView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                Spacer()
-                        .frame(minHeight: 0, maxHeight: 16) 
-                Picker("", selection: $selectedTabIndex) {
-                    ForEach(0..<TabSelection.allCases.count, id: \.self) { index in
-                        Text(TabSelection.allCases[index].rawValue.capitalized)
+            ZStack {
+                appSettings.isDarkMode ? LinearGradient(Color.darkStart, Color.appBlacks).ignoresSafeArea() : Color.linearWhiteColor.ignoresSafeArea()
+                VStack {
+                    Spacer()
+                        .frame(minHeight: 0, maxHeight: 16)
+                    Picker("", selection: $selectedTabIndex) {
+                        ForEach(0..<TabSelection.allCases.count, id: \.self) { index in
+                            Text(TabSelection.allCases[index].rawValue.capitalized)
+                        }
                     }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding(.horizontal)
+                    if !filteredData.isEmpty {
+                        ICCRankingView(selectedTab: selectedTab, rankingArray: filteredData)
+                    } else {
+                        Spacer()
+                        LoadingIndicator()
+                    }
+                    
+                    Spacer()
                 }
-                .pickerStyle(SegmentedPickerStyle())
-                .padding(.horizontal)
-                if !filteredData.isEmpty {
-                    ICCRankingView(selectedTab: selectedTab, rankingArray: filteredData)
-                } else {
-                    ProgressView()
+                .onAppear {
+                    self.rankingViewModel.fetchRankings(self.matchFormat)
                 }
-                
-                Spacer()
+                .navigationBarTitle("ICC Rankings")
+                .navigationBarBackButtonHidden(true)
+                .toolbar(content: {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        CustomBackButton()
+                    }
+                })
             }
-            .onAppear {
-                self.rankingViewModel.fetchRankings(self.matchFormat)
-            }
-            .navigationBarTitle("ICC Rankings")
-            .navigationBarBackButtonHidden(true)
-            .toolbar(content: {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    CustomBackButton()
-                }
-            })
         }
+        .scrollContentBackground(.hidden)
     }
 }
 
